7980cd81f25a191fdec8294d120117655336c02e
Ian Gopen <iangopen@Ians-Laptop-2.local>
2026-05-14 09:17:01 -0700

    bugfix - critical texture fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-    };
-});
+export default defineConfig({
+  plugins: [react()],
+  base: '/nauticalmaster/',
+})
